f2b8d66ec139453252c5a2a39cf61356cee7bc3b
Rafi Fathia <rafifathia@gmail.com>
2026-05-13 09:26:56 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,28 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 int main (){
-    int i, j, r, c, curr, visited_dfs;
-    printf("enter the row value : ");
+    int i, j, r, c;
     scanf("%d", &r);
-    printf("enter the column value : ");
     scanf("%d", &c);
 
     int mat[r][c];
     
     for(i = 0; i < r; ++i){
         for(j = 0; j < c ; ++j) {
-            printf("matriks [%d][%d]:", i, j);
             scanf("%d", &mat[i][j]);
         }
-    }
-
-    printf("grid peta nya :\n");
-    for(i = 0; i < r; ++i){
-    for(j = 0; j < c ; ++j){
-           printf("%d\t",mat[i][j]);
-        }
-    printf("\n");
     }
 
     printf("ISLANDS 0");
