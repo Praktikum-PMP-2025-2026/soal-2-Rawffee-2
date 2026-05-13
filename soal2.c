e7276bec41b80bcf52ab0624f64860422c2df572
Rafi Fathia <rafifathia@gmail.com>
2026-05-13 10:59:21 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,6 +1,10 @@
-// Praktikum EL2008 PMP
+// Praktikum EL2008 Pemecahan Masalah dan Pemograman 2025/2026
+// Modul : 5
 // Nama : Muhammad Rafi Fathia Dzaky
 // NIM  : 13224079
+// file : soal2.c
+// Deskripsi : soal kedua tentang pulau-pulau menggunakan DFS
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
