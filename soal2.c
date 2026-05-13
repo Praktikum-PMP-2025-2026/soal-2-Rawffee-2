0971dfc021da2ba7904630ed4ed551188a64effd
Rafi Fathia <rafifathia@gmail.com>
2026-05-13 10:50:21 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -5,38 +5,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int dx[] = {-1, 0, 1, 0, 1, -1, -1, 1};
-int dy[] = {0, -1, 0, 1, 1, 1, -1, -1};
-
-void dfs(int mat, int r, int c, int n, int m) {
-    if (r < 0 || c < 0 || r >= n || c >= m || mat[r][c] != 1)
-        return;
-
-    mat[r][c] = 0;
-
-
-    for (int i = 0; i < r; i++) {
-        int newR = r + dx[i];
-        int newC = c + dy[i];
-        dfs(mat, newR, newC, n, m);
-    }
-}
-
-int countIslands(int mat, int r, int m) {
-    int count = 0;
-
-    for (int r = 0; r < n; r++) {
-        for (int c = 0; c < m; c++) {
-            if (mat[r][c] == 'L') {
-                count++;
-                dfs(mat, r, c, n, m);
-            }
-        }
-    }
-
-    return count;
-}
-
 int main (){
     int i, j, r, c;
     scanf("%d", &r);
@@ -54,7 +22,8 @@ int main (){
     printf("LARGEST 0");
 
 return 0;
-};
+}
+
 //source : https://www.geeksforgeeks.org/dsa/depth-first-traversal-dfs-on-a-2d-array/
 //source : https://www.geeksforgeeks.org/dsa/print-matrix-elements-using-dfs-traversal/
 //source : https://www.geeksforgeeks.org/dsa/breadth-first-traversal-bfs-on-a-2d-array/
