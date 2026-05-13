d8ac298294ef1384528f33f5843d9851ae5938f0
Rafi Fathia <rafifathia@gmail.com>
2026-05-13 10:20:57 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,3 +1,6 @@
+// Praktikum EL2008 PMP
+// Nama : Muhammad Rafi Fathia Dzaky
+// NIM  : 13224079
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -20,3 +23,7 @@ int main (){
 
 return 0;
 };
+
+//source : https://www.geeksforgeeks.org/dsa/depth-first-traversal-dfs-on-a-2d-array/
+//source : https://www.geeksforgeeks.org/dsa/print-matrix-elements-using-dfs-traversal/
+//source : https://www.geeksforgeeks.org/dsa/breadth-first-traversal-bfs-on-a-2d-array/
